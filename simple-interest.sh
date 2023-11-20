96ac1ffc0e74460be517e03879037ba413957921
Yuhan Zhang <72619613+yhnnii@users.noreply.github.com>
2023-11-20 11:32:14 -0500

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+   #!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <your GitHub username>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
